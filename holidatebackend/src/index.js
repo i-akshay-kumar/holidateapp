9e0bd392ccb00194efb5b03809a0c2254d41e92e
akshay <ak5967751@gmail.com>
2023-01-11 19:05:50 +0530

    ninth commit

--- holidatebackend/src/index.js
+++ holidatebackend/src/index.js
@@ -7,9 +7,13 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-app.use(cors({
-    origin : "*"
-}));
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "../../holidate/dist")))
