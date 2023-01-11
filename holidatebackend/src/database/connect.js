2d117a83f4f75e88c1f92cacaca09289cd5165a5
akshay <ak5967751@gmail.com>
2023-01-11 17:30:20 +0530

    fifth commit

--- holidatebackend/src/database/connect.js
+++ holidatebackend/src/database/connect.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/holidateusers").then(()=>{
+mongoose.connect("mongodb+srv://ak5967751:asdfghjas@cluster0.glxygxz.mongodb.net/holidateusers?retryWrites=true&w=majority", {
+    useNewUrlParser : true,  
+    useUnifiedTopology : true
+}).then(()=>{
     console.log("connection established");
 }).catch((err)=>{
     console.log(err);
